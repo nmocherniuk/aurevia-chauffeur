66ef57288075144f7ca1abfb04467739a3696cf8
Nazar <nazar.mocherniuk@gmail.com>
2026-03-08 23:24:33 +0200

    feat: enhance Button component styling with transition effects
    - Updated Button component styles to include transition effects for hover states, improving interactivity and user experience.
    - Adjusted CSS classes for primary, secondary, muted, tab, and tabActive variants to enhance visual feedback on user actions.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -18,14 +18,14 @@ export const Button: FC<ButtonProps> = ({
 }) => {
   const variants = {
     primary:
-      "bg-primary text-white rounded-lg py-2 h-[42px] px-5 flex items-center justify-center gap-2 cursor-pointer",
+      "bg-primary text-white rounded-lg py-2 h-[42px] px-5 flex items-center justify-center gap-2 cursor-pointer transition-all duration-300 hover:bg-[#AC8458]",
     secondary:
-      "border border-primary text-text-secondary bg-transparent rounded-lg py-2 h-[42px] px-5 cursor-pointer",
+      "border border-primary text-primary bg-transparent rounded-lg py-2 h-[42px] px-5 cursor-pointer transition-all duration-300 hover:border-text-secondary hover:text-text-secondary",
     muted:
-      "border border-grey text-grey bg-transparent rounded-md py-2 h-[42px] px-5 cursor-pointer",
-    tab: "border border-grey text-grey rounded-md px-5 h-[42px] cursor-pointer transition-colors duration-300",
+      "border border-grey text-grey bg-transparent rounded-md py-2 h-[42px] px-5 cursor-pointer transition-colors duration-300 hover:border-grey-light hover:text-grey-light hover:bg-grey/10",
+    tab: "border border-grey text-grey rounded-md px-5 h-[42px] cursor-pointer transition-colors duration-300 hover:border-text-secondary hover:text-text-secondary",
     tabActive:
-      "border border-primary bg-primary text-white rounded-md px-5 h-[42px] cursor-pointer transition-colors duration-300",
+      "border border-primary bg-primary text-white rounded-md px-5 h-[42px] cursor-pointer transition-all duration-300 hover:bg-[#AC8458]",
   };
 
   return (
