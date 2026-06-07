628074b949141b4d87e157ab1634d2fe4a2acce8
Nazar <nazar.mocherniuk@gmail.com>
2026-06-08 02:00:09 +0300

    feat: add remote image pattern for optimized loading
    - Configured Next.js to allow images from "https://api.riviera-prime.com/uploads/**", enhancing image loading capabilities and performance.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.riviera-prime.com",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
